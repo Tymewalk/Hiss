28d48dd681dd20d839aa0748ee478947a0eb3da0
Tymewalk <Tymewalk@users.noreply.github.com>
2016-03-11 10:51:22 -0600

    Split Sprite and Stage, begin code
    The way I see it, sprites should be an extension of the stage. After all, the stage is like one big sprite that can't move.
    Also, begin code has been added.

--- hiss/__init__.py
+++ hiss/__init__.py
@@ -1,10 +1,22 @@
-import pygame
-
-class Sprite():
+import pygame, time, random
+        
+class Stage():
     def __init__(self, name):
         self.name = name
         self.costumes = []
 
     def addCostume(self, costumePath):
         costume = pygame.image.load(costumePath)
-        self.costumes.append(costume)
+        self.costumes.append(costume) 
+
+class Sprite(Stage):
+    def __init__(self, name):
+        Stage.__init__(self, name)
+        self.xpos = 0
+        self.ypos = 0
+    
+def beginGame():
+    pygame.init()
+
+    screen = pygame.display.set_mode((800, 600)) # Add customizable dimensions later on?
+    caption = pygame.display.set_caption("Hiss Project")
