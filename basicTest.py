dc79be637859a8cfb8fc67a62bea1d3239f0e8a4
Tymewalk <Tymewalk@users.noreply.github.com>
2016-03-11 16:43:22 -0600

    Do the import right
    I think this syntax is better and clearer.
    Still can't get the background color to change though.

--- basicTest.py
+++ basicTest.py
@@ -1,14 +1,14 @@
-from hiss import *
+import hiss
 
-newSprite = Sprite()
+newSprite = hiss.Sprite()
 newSprite.addCostume("SOS.png")
 newSprite.currentCostume = newSprite.costumes[0] # TODO: Make "change costume" block
 
-hissStage.bgcolor = (255, 255, 255)
+hiss.hissStage.bgcolor = (255, 255, 255)
 
 continueLoop = True
 while continueLoop:
-    blit() # Display
+    hiss.blit() # Display
     # Handle quitting
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
