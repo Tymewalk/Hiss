e7ea4010c2c296fd29bd9e3f21226f6e795ce7fb
Tymewalk <Tymewalk@users.noreply.github.com>
2016-03-13 14:48:59 -0500

    Better test It has two sprites and uses the new goTo() function. Next up - making a size function!

--- basicTest.py
+++ basicTest.py
@@ -1,15 +1,23 @@
 import hiss, pygame, time
 
-newSprite = hiss.Sprite()
-newSprite.addCostume("SOS.png")
-newSprite.setCostumeByNumber(0) # TODO: Make "change costume" block
+SOS = hiss.Sprite()
+SOS.addCostume("SOS.png")
+SOS.setCostumeByNumber(0)
+
+SoExcited = hiss.Sprite()
+SoExcited.addCostume("SoExcited.png")
+SoExcited.setCostumeByNumber(0)
+
+SoExcited.goTo(300, 300)
+
+
 
 hiss.hissStage.setColor(255, 0, 0)
 
 continueLoop = True
 while continueLoop:
     hiss.blit() # Display
-    newSprite.changeXBy(1)
+    SOS.changeXBy(1)
     # Handle quitting
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
