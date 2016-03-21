1ac853f7beca0f47a0548c9b664ffecaf2925c0f
Tymewalk <Tymewalk@users.noreply.github.com>
2016-03-21 12:05:26 -0500

    Fix test and comments basicTest.py now has more comments and a fix.

--- basicTest.py
+++ basicTest.py
@@ -1,19 +1,18 @@
 import slither, pygame, time
 
-
 snakey = slither.Sprite()
-snakey.setCostumeByName("costume0")
+snakey.setCostumeByName("costume0") # You can set the default costume (costume0) by name...
 
 SoExcited = slither.Sprite()
 SoExcited.addCostume("SoExcited.png", "avatar")
-SoExcited.setCostumeByNumber(0)
+SoExcited.setCostumeByNumber(0) # ...or you can use a number.
 
 SoExcited.goTo(300, 300)
-SoExcited.setScaleTo(0.33)
+SoExcited.setScaleTo(0.33) # May look grainy when used on too low a scale.
 
 slither.slitherStage.setColor(40, 222, 40)
 
-screen = slither.setup() # Begin slither
+slither.setup() # Begin slither
 
 def run_a_frame():
     snakey.changeXBy(1)
