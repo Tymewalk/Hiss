2b8aaf361b6e085c6cc0c5a5ba9c670c804bc8aa
Matthew <BookOwl@users.noreply.github.com>
2016-03-21 12:00:37 -0400

    Add #10
    turnAndMove2.py uses the new API

---
+++ turnAndMove2.py
@@ -0,0 +1,26 @@
+import slither, pygame
+
+snakey = slither.Sprite()
+snakey.setCostumeByName("costume0")
+
+snakey.goTo(0, 0)
+
+slither.slitherStage.setColor(40, 222, 40)
+
+slither.setup() # Begin slither
+
+def handlequit():
+    print("Quiting...")
+    return True
+slither.registerCallback(pygame.quit, handlequit) # This uses the direct call form
+
+@slither.registerCallback(pygame.MOUSEBUTTONUP) # This uses the decorator form
+def handlemouseup(event):
+    print("Mouseup:", event.pos, event.button)
+
+def run_a_frame():
+    snakey.changeXBy(1)
+    snakey.changeYBy(1)
+    snakey.changeDirectionBy(1)
+
+slither.runMainLoop(run_a_frame)
