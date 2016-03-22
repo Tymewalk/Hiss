da6847fc3eeadb517c62280c79ca4f41242e13f2
Tymewalk <Tymewalk@users.noreply.github.com>
2016-03-22 11:28:10 -0500

    Move turnAndMove2.py The old turnAndMove doesn't work anymore, and this one was made to replace the old one.

--- turnAndMove.py
+++ turnAndMove.py
@@ -1,5 +1,4 @@
-import slither, pygame, time
-
+import slither, pygame
 
 snakey = slither.Sprite()
 snakey.setCostumeByName("costume0")
@@ -8,16 +7,20 @@ snakey.goTo(0, 0)
 
 slither.slitherStage.setColor(40, 222, 40)
 
-screen = slither.setup() # Begin slither
+slither.setup() # Begin slither
 
-continueLoop = True
-while continueLoop:
-    slither.blit(screen) # Display
+def handlequit():
+    print("Quiting...")
+    return True
+slither.registerCallback(pygame.QUIT, handlequit) # This uses the direct call form
+
+@slither.registerCallback(pygame.MOUSEBUTTONUP) # This uses the decorator form
+def handlemouseup(event):
+    print("Mouseup:", event.pos, event.button)
+
+def run_a_frame():
     snakey.changeXBy(1)
     snakey.changeYBy(1)
     snakey.changeDirectionBy(1)
-    # Handle quitting
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            continueLoop = False
-    time.sleep(0.01)
+
+slither.runMainLoop(run_a_frame)
