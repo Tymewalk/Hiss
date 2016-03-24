3d0db248ef89d4b6db48f1153279feefaabb3423
Matthew <BookOwl@users.noreply.github.com>
2016-03-24 14:43:23 -0400

    Add a higher res background image

--- examples/bgTest.py
+++ examples/bgTest.py
@@ -1,9 +1,9 @@
 import slither, pygame, time, os, math
 snakey = slither.Sprite()
 snakey.setCostumeByName("costume0")
-snakey.setXTo(300)
+snakey.setYTo(360)
 slither.slitherStage.setColor(40, 222, 40)
-slither.slitherStage.addCostume(os.path.join(os.path.dirname(__file__), "bg.png"), "bg")
+slither.slitherStage.addCostume(os.path.join(os.path.dirname(__file__), "bg2.png"), "bg")
 slither.slitherStage.setCostumeByName("bg")
 
 slither.setup() # Begin slither'
@@ -11,7 +11,7 @@ slither.setup() # Begin slither'
 i = 0
 def run_a_frame():
     global i
-    snakey.setYTo((math.sin(i) * 200) + 200)
+    snakey.setXTo((math.sin(i) * 200) + 300)
     i += 0.05
 
 slither.runMainLoop(run_a_frame)
