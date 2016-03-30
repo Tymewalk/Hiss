dccd3a78451a41efd71cf01ecbe1d6626d82fc9a
Matthew <BookOwl@users.noreply.github.com>
2016-03-30 16:33:43 -0400

    Update imageBounceTest.py to the new API

--- examples/imageBounceTest.py
+++ examples/imageBounceTest.py
@@ -1,16 +1,25 @@
 import slither
 
-circle = slither.Sprite()
-circle.addCostume("circle.png", "circle")
-circle.setCostumeByName("circle")
-circle.goto(0,0)
+def create_circle():
+    circle = slither.Sprite()
+    circle.addCostume("circle.png", "circle")
+    circle.setCostumeByName("circle")
+    circle.goto(0,0)
+    return circle
+circle1, circle2, circle3 = create_circle(), create_circle(), create_circle()
 
-slither.slitherStage.setColor(255, 255, 255)
+slither.slitherStage.bgcolor = (255, 255, 255)
 
 def run_a_frame():
-    circle.xpos += 1
-    circle.ypos += 1
-    circle.direction += 1
+    circle1.xpos += 1
+    circle1.ypos += 1
+    circle1.direction += 1
+
+    circle2.xpos += 1
+    circle2.direction += 1
+
+    circle3.ypos += 1
+    circle3.direction += 1
 
 slither.setup()
 slither.runMainLoop(run_a_frame)
