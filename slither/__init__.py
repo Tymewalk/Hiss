5666d4acf2498ccc3a53354a2fc4c5fc9354809c
Tymewalk <Tymewalk@users.noreply.github.com>
2016-06-01 11:35:11 -0500

    Add parentheses to end of get_rect Whoops. I was setting the variable with the function rather than calling the function.

--- slither/__init__.py
+++ slither/__init__.py
@@ -138,8 +138,8 @@ class Sprite(Stage):
 
     def isTouching(self, collideSprite):
         '''Detects if one sprite is touching another.'''
-        ourRect = self.currentCostume.get_rect
-        theirRect = collideSprite.currentCostume.get_rect
+        ourRect = self.currentCostume.get_rect()
+        theirRect = collideSprite.currentCostume.get_rect()
 
         return ourRect.colliderect(theirRect)
 
