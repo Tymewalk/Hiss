f17fef4cf07c16edd7d87cf564c275870b562589
BookOwl <stanleybookowl@gmail.com>
2016-06-02 16:06:58 -0400

    Fix print statements in isTouchingTest.py

--- slither/tests/isTouchingTest.py
+++ slither/tests/isTouchingTest.py
@@ -12,9 +12,9 @@ snakey.goto(slither.WIDTH // 2, 100)
 def run_a_frame():
     toucher.moveSteps(3)
     if toucher.isTouching(snakey):
-        print "Touching!"
+        print("Touching!")
     else:
-        print "Not touching."
+        print("Not touching.")
 
 slither.setup()
 slither.runMainLoop(run_a_frame)
