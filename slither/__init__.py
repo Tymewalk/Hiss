63787a7f412a20f08d9c06cc756535c7b15ad75b
Tymewalk <Tymewalk@users.noreply.github.com>
2016-06-03 12:47:34 -0500

    Add whitespace and fix spelling error Just to make it look prettier. If for some reason the whitespace breaks things go ahead and remove it.

--- slither/__init__.py
+++ slither/__init__.py
@@ -40,48 +40,62 @@ class Mouse:
     """A class for getting and setting mouse properties
     This is a static class, all functions should be called directly through the class"""
     _v = True
+    
     @staticmethod
     def buttonsPressed():
         """Returns a three-tuple of bools that gives the state 
         of the left, middle, and right buttons"""
         return tuple(bool(state) for state in pygame.mouse.get_pressed())
+    
     @staticmethod
     def leftPressed():
         return Mouse.buttonsPressed()[0]
+    
     @staticmethod
     def middlePressed():
         return Mouse.buttonsPressed()[1]
+    
     @staticmethod
     def rightPressed():
         return Mouse.buttonsPressed()[2]
+    
     @staticmethod
     def pos():
         return pygame.mouse.get_pos()
+    
     @staticmethod
     def xPos():
         return Mouse.pos()[0]
+    
     @staticmethod
     def yPos():
         return Mouse.pos()[1]
+    
     @staticmethod
     def relativeMovement():
         "Returns how much the mouse has moved since the last call to this function"
         return pygame.mouse.get_rel()
+    
     @staticmethod
     def setPos(x, y):
         pygame.mouse.set_pos(x, y)
+        
     @staticmethod
     def setXPos(x):
         pygame.mouse.set_pos(x, Mouse.yPos())
+        
     @staticmethod
     def setYPos(y):
         pygame.mouse.set_pos(Mouse.xPos(), y)
+        
     @staticmethod
-    def isVisable():
+    def isVisible():
         return Mouse._v
+    
     @staticmethod
-    def setVisable(status):
+    def setVisible(status):
         Mouse._v = pygame.mouse.set_visible(status)
+        
     @staticmethod
     def isFocused():
         return bool(pygame.mouse.get_focused())
